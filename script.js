eaf149149a6de83e6ce800c03b74c73a4b18e476
RODRIGO BERGENTHAL <116042656+RodrigoBergenthal@users.noreply.github.com>
2025-09-08 14:38:11 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Este script pode ser expandido para adicionar interatividade.
+// Por enquanto, apenas um exemplo simples de alerta ao clicar no botão.
+
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    alert('Obrigado pelo interesse! Em breve, mais informações sobre como participar do programa de pontos.');
+});
